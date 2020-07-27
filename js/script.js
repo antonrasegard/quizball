6971acffe6d2f9b7689f2e26629b89926c0b4de8
antonrasegard <arasegard@gmail.com>
2020-07-27 11:30:15 +0000

    Added possbility to select correct answer and move on

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const playButton = document.getElementById('play-btn')
+const firstdownButton = document.getElementById('firstdown-btn')
 const quizContainerElement = document.getElementById ('quiz-container')
 
 const questionElement = document.getElementById('question')
@@ -18,22 +19,54 @@ function playGame() {
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
-function selectAnswer() {
+function resetState() {
+    firstdownButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
 }
 
 const questions = [
     {
-        question: 'Which player has won the most Super Bowls with the same team?',
+        question: 'Who of these players has won the most Super Bowls with the same team?',
         answers: [
-            { text: 'Broadway Joe Nemeth', correct: false },
+            { text: '"Broadway Joe" Nemeth', correct: false },
             { text: 'Franco Harris', correct: false },
             { text: 'Tom Brady', correct: true },
             { text: 'Nathan Peterman', correct: false },
