abfa95588783777286333bd37d774d2bc4a5d06a
antonrasegard <arasegard@gmail.com>
2020-07-27 10:47:49 +0000

    Changed bg-theme in accordance with mentor. Made js functional in transition between start pae and first question

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-const playButton = document.getElementById('play-btn');
-const quizContainerElement = document.getElementById ('quiz-container');
+const playButton = document.getElementById('play-btn')
+const quizContainerElement = document.getElementById ('quiz-container')
 
-playButton.addEventListener('click', playGame);
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+playButton.addEventListener('click', playGame)
 
 function playGame() {
-    playButton.classList.add('hide');
-    quizContainerElement.classList.remove('hide');
-    setNextQuestion();
-};
+    console.log('Started')
+    playButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    quizContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 function selectAnswer() {
@@ -21,13 +33,12 @@ const questions = [
     {
         question: 'Which player has won the most Super Bowls with the same team?',
         answers: [
-            { text: 'Broadway Joe Nemeth' correct: false };
-            { text: 'Franco Harris' correct: false };
-            { text: 'Tom Brady' correct: true };
-            { text: 'Nathan Peterman' correct: false };
-            { text: 'Marky Mark' correct: false };
-            { text: 'Terrell Suggs' correct: false };
+            { text: 'Broadway Joe Nemeth', correct: false },
+            { text: 'Franco Harris', correct: false },
+            { text: 'Tom Brady', correct: true },
+            { text: 'Nathan Peterman', correct: false },
+            { text: 'Marky Mark', correct: false },
+            { text: 'Terrell Suggs', correct: false }
         ]
     }
 ]
-
